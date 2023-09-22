d7c34997d1c4d75785f00606d6c068dbb0f44f32
Gabriele Grillo <gabriele.grillo.2003@gmail.com>
2023-09-22 11:26:29 +0200

    Check availbility on modules.

--- RemoveYourselfFromTeam.ps1
+++ RemoveYourselfFromTeam.ps1
@@ -1,19 +1,26 @@
-# $InputTeam = Read-Host "Write the name of the team that you want to remove yourself:"
+# TODO:
+# Paramaters 
+# check if it works on windows
+if (Get-Module -ListAvailable -Name PowerShellGet -and Get-Module -ListAvailable -Name MicrosoftTeams) {
+    Write-Host "OK!"
+}
+else {
+    Install-Module -Name PowerShellGet -Force -AllowClobber
+    Install-Module -Name MicrosoftTeams -Force -AllowClobber
+    Exit
+}
+
+# Connect and get the email.
 $acc = Connect-MicrosoftTeams | Select-Object -ExpandProperty "Account"
-Write-Host "Login effettuato con:" $acc
+Write-Host "Logged in with:" $acc
 $all_teams = Get-Team -User $acc
 
 for ($i = 0; $i -lt $all_teams.Length; $i++) {
     Write-Host $i")" $all_teams[$i].DisplayName
 }
 
-# $Group = Get-Team -DisplayName "$InputTeam" | Select-Object -ExpandProperty "GroupId"
-# Remove-TeamUser -GroupId $Grodxup -User $Account
-# Disconnect-MicrosoftTeams
-
-$choice = Read-Host "Select the team where you want to remove yourself:"
+$choice = Read-Host "Select the team you want to remove yourself from: "
 $groupId = $all_teams[$choice].GroupId 
-# $Group = Get-Team -DisplayName "$InputTeam" | Select-Object -ExpandProperty "GroupId"
 Remove-TeamUser -GroupId $groupId -User $acc
 Disconnect-MicrosoftTeams
 
